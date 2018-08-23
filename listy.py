3f0525f47e1aac113518c5f6a41172ea089b8725
haxmanster <grzegorz-wolyniec@o2.pl>
2018-08-23 13:06:42 +0200

    Refactor code

--- listy.py
+++ listy.py
@@ -4,6 +4,7 @@ import pickle
 def dodaj_przedszkolaka(option):
     with open('parrot.pkl', 'rb') as f:
         listen = pickle.load(f)
+
     if option == "show":
         print("Przedszkolaki znajdujace sie na liscie: ", listen)
 
@@ -11,18 +12,17 @@ def dodaj_przedszkolaka(option):
         ile = input("Ilu przedszkolaków chcesz dodac do listy ? ")
         for imie in range(int(ile)):
             listen.append(input("Imie Przedszkolaka "))
-            with open('parrot.pkl', 'wb') as f:
-                pickle.dump(listen, f)
+            pickle.dump(listen, f)
         print(listen)
 
     if option == "remove":
-        with open('parrot.pkl', 'rb') as f:
-            listen = pickle.load(f)
+        listen = pickle.load(f)
         ile = input("Ilu przedszkolaków chcesz usunac z  listy ? ")
         for usun in range(int(ile)):
             listen.remove(input("imie przedszkolaka "))
             print(listen)
     return
+
 
 print("""
     Wpisz opcje ktore chciałbyś wykonać w naszym programie:
