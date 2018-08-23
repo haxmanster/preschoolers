ea5abf6783fd83c49694d1db27dcbf578d57e8fb
haxmanster <grzegorz-wolyniec@o2.pl>
2018-08-23 13:16:59 +0200

    Crate moduls function

---
+++ function.py
@@ -0,0 +1,25 @@
+import pickle
+
+def dodaj_przedszkolaka(option):
+    with open('parrot.pkl', 'rb') as f:
+        listen = pickle.load(f)
+    if option == "show":
+        print("Przedszkolaki znajdujace sie na liscie: ", listen)
+
+    if option == "add":
+        ile = input("Ilu przedszkolaków chcesz dodac do listy ? ")
+        for imie in range(int(ile)):
+            listen.append(input("Imie Przedszkolaka "))
+            with open('parrot.pkl', 'wb') as f:
+                pickle.dump(listen, f)
+        print(listen)
+
+    if option == "remove":
+        with open('parrot.pkl', 'rb') as f:
+            listen = pickle.load(f)
+        ile = input("Ilu przedszkolaków chcesz usunac z  listy ? ")
+        print(listen)
+        for usun in range(int(ile)):
+            listen.remove(input("imie przedszkolaka "))
+            print(listen)
+    return
--- listy.py
+++ listy.py
@@ -1,28 +1,5 @@
-import pickle
 
-
-def dodaj_przedszkolaka(option):
-    with open('parrot.pkl', 'rb') as f:
-        listen = pickle.load(f)
-
-    if option == "show":
-        print("Przedszkolaki znajdujace sie na liscie: ", listen)
-
-    if option == "add":
-        ile = input("Ilu przedszkolaków chcesz dodac do listy ? ")
-        for imie in range(int(ile)):
-            listen.append(input("Imie Przedszkolaka "))
-            pickle.dump(listen, f)
-        print(listen)
-
-    if option == "remove":
-        listen = pickle.load(f)
-        ile = input("Ilu przedszkolaków chcesz usunac z  listy ? ")
-        for usun in range(int(ile)):
-            listen.remove(input("imie przedszkolaka "))
-            print(listen)
-    return
-
+from function import dodaj_przedszkolaka
 
 print("""
     Wpisz opcje ktore chciałbyś wykonać w naszym programie:
@@ -31,4 +8,5 @@ print("""
         remove - usuwa uczniow do listy
         show - pokaz uczniow na liscie 
 """)
+
 dodaj_przedszkolaka(input())
