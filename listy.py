1e6490a16479e1874a7d325e04fdf4dd271ed0a2
haxmanster <grzegorz-wolyniec@o2.pl>
2018-08-23 12:58:30 +0200

    adding few option

--- listy.py
+++ listy.py
@@ -2,7 +2,11 @@ import pickle
 
 
 def dodaj_przedszkolaka(option):
-    listen = []
+    with open('parrot.pkl', 'rb') as f:
+        listen = pickle.load(f)
+    if option == "show":
+        print("Przedszkolaki znajdujace sie na liscie: ", listen)
+
     if option == "add":
         ile = input("Ilu przedszkolaków chcesz dodac do listy ? ")
         for imie in range(int(ile)):
@@ -14,11 +18,17 @@ def dodaj_przedszkolaka(option):
     if option == "remove":
         with open('parrot.pkl', 'rb') as f:
             listen = pickle.load(f)
-        print(listen)
         ile = input("Ilu przedszkolaków chcesz usunac z  listy ? ")
         for usun in range(int(ile)):
             listen.remove(input("imie przedszkolaka "))
             print(listen)
     return
 
-dodaj_przedszkolaka(input("Podaj opcje 'add' albo 'remove' "))
+print("""
+    Wpisz opcje ktore chciałbyś wykonać w naszym programie:
+        
+        add - dodaje uczniów do listy
+        remove - usuwa uczniow do listy
+        show - pokaz uczniow na liscie 
+""")
+dodaj_przedszkolaka(input())
